ce9d4ee2effc5ce3a892377316ab07775950d2bf
Wille Marcel <wille.yyz@gmail.com>
2023-05-16 15:37:22 -0300

    Add MilvusConnection class

--- bioacoustics/milvus/connection.py
+++ bioacoustics/milvus/connection.py
@@ -3,16 +3,46 @@ from pymilvus import connections, Collection
 from django.conf import settings
 
 
-class Connection:
+class MilvusConnection:
+    def __init__(self):
+        self.connect()
+        self.collection = self.get_main_collection()
+        self.collection.load()
+
     def connect(self):
         MILVUS = settings.MILVUS
         connections.connect(
-            alias=MILVUS.DB_ALIAS,
-            user=MILVUS.DB_USER,
-            password=MILVUS.DB_PASSWORD,
-            host=MILVUS.DB_HOST,
-            port=MILVUS.DB_PORT,
+            # alias=MILVUS['DB_ALIAS'],
+            # user=MILVUS['DB_USER'],
+            # password=MILVUS['DB_PASSWORD'],
+            host=MILVUS['DB_HOST'],
+            port=MILVUS['DB_PORT'],
         )
 
     def get_collection(self, collection_name):
         return Collection(name=collection_name)
+
+    def get_main_collection(self):
+        return Collection(name=settings.MILVUS['DB_COLLECTION'])
+
+    def drop_index(self, collection_name):
+        self.get_collection(collection_name).drop_index()
+
+    def create_index(self, field, index_params):
+        self.collection.create_index(field, index_params)
+        print(f"Created index {self.collection.index().params}")
+
+    def search(self, query_vector, expression=None, limit=10):
+        # Search the Milvus collection for similar vectors
+        search_params = {
+            "data": query_vector,
+            "anns_field": "embedding",
+            "param": {"metric_type": "L2", "params": {"nprobe": 16}},
+            "limit": limit,
+            "output_fields": ["site_name", "subsite_name", "file_timestamp"]
+        }
+        if expression:
+            search_params['expr'] = expression
+
+        results = self.collection.search(**search_params)
+        return results
