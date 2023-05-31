fa97b2fc77c9db01bcee52afbde1eb2ad4e3eabd
Wille Marcel <wille.yyz@gmail.com>
2023-05-30 22:44:42 -0300

    Add openapi schema generation (#16)

--- bioacoustics/urls.py
+++ bioacoustics/urls.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+from rest_framework.permissions import AllowAny
+from rest_framework.schemas import get_schema_view
 
 from .users.views import UserViewSet, UserCreateViewSet
 from .milvus.views import search_view
@@ -19,6 +21,12 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api/v1/a2o/', include(urlpatterns)),
     path('api/v1/search/', search_view),
+    path('api/v1/openapi/', get_schema_view(
+        title="Google Bioacoustics API",
+        description="API for Google Bioacoustics project",
+        version="1.0.0",
+        permission_classes=[AllowAny]
+    ), name='openapi-schema'),
     path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
