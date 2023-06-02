43469df7c8a558abc8fec8ec767fc4749da0c32e
Wille Marcel <wille.yyz@gmail.com>
2023-06-02 07:57:25 -0300

    Add documentation and cache to a2o proxy views (#19)

--- bioacoustics/a2o_api_proxy/views.py
+++ bioacoustics/a2o_api_proxy/views.py
@@ -1,7 +1,7 @@
 from urllib.parse import urljoin
 
 from django.conf import settings
-from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
@@ -11,21 +11,27 @@ import requests
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
+@cache_page(60*60*4)  # 4 hours of cache
 def sites_list(request):
+    """Get the list of sites from the Acoustic Observatory data API."""
     status, data = make_request('sites/')
     return Response(data, status)
 
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
+@cache_page(60*60*4)  # 4 hours of cache
 def sites_detail(request, id):
+    """Get the detail about a site from the Acoustic Observatory data API."""
     status, data = make_request('sites/{}'.format(id))
     return Response(data, status)
 
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
+@cache_page(60*60)  # 1 hour of cache
 def recordings_detail(request, id):
+    """Get information about an Audio Recording from the Acoustic Observatory data API."""
     status, data = make_request('audio_recordings/{}'.format(id))
     return Response(data, status)
 
