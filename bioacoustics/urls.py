c49226dacb8bb591c39dd50dca638db56a58a350
Wille Marcel <wille.yyz@gmail.com>
2023-07-10 20:59:06 -0300

    Improve API-Docs

--- bioacoustics/urls.py
+++ bioacoustics/urls.py
@@ -7,6 +7,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from rest_framework.permissions import AllowAny
 from rest_framework.schemas import get_schema_view
+from drf_spectacular.views import SpectacularAPIView
 
 from .users.views import UserViewSet, UserCreateViewSet
 from .milvus.views import search_view, embed_view
@@ -22,12 +23,7 @@ urlpatterns = [
     path('api/v1/a2o/', include(urlpatterns)),
     path('api/v1/search/', search_view),
     path('api/v1/embed/', embed_view),
-    path('api/v1/openapi/', get_schema_view(
-        title="Google Bioacoustics API",
-        description="API for Google Bioacoustics project",
-        version="1.0.0",
-        permission_classes=[AllowAny]
-    ), name='openapi-schema'),
+    path('api/v1/openapi/', SpectacularAPIView.as_view(), name='openapi-schema'),
     path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
